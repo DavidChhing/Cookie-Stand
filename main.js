f4cbeb09e78552a927995d5bdc44e8eb68621707
Davidchhing <davidhchhing@gmail.com>
2018-02-13 19:51:57 -0800

    created js

---
+++ main.js
@@ -0,0 +1,6 @@
+var cookieStand = [];
+cookieStand.push({location: "Pioneer Square", minCust: "17", maxCust: "88", avgCookieSale:"5.2"});
+cookieStand.push({location: "Portland Airport", minCust: "6", maxCust: "24", avgCookieSale: "1.2"});
+cookieStand.push({location: "Washington Square", minCust: "11", maxCust: "38", avgCookieSale: "1.9"});
+cookieStand.push({location: "Sellwood", minCust: "20", maxCust: "48", avgCookieSale: "3.3"});
+cookieStand.push({location: "Pearl District", minCust: "3", maxCust: "24", avgCookieSale: "2.6"});
