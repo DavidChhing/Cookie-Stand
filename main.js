9611641cd92d7665da251529640f31a78d8278e3
Davidchhing <davidhchhing@gmail.com>
2018-02-18 09:31:19 -0800

    updated pioneer square with push

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-var cookieStand = [];
-cookieStand.push({location: "Pioneer Square", minCust: "17", maxCust: "88", avgCookieSale:"5.2"});
-cookieStand.push({location: "Portland Airport", minCust: "6", maxCust: "24", avgCookieSale: "1.2"});
-cookieStand.push({location: "Washington Square", minCust: "11", maxCust: "38", avgCookieSale: "1.9"});
-cookieStand.push({location: "Sellwood", minCust: "20", maxCust: "48", avgCookieSale: "3.3"});
-cookieStand.push({location: "Pearl District", minCust: "3", maxCust: "24", avgCookieSale: "2.6"});
 
+var timeOfDay = ['10AM', '11AM', '12pm', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
+    
 
 var pioneerSquare = {
     location: "Pioneer Square",
@@ -16,9 +12,19 @@ var pioneerSquare = {
         return Math.floor(Math.random() * (this.maxCust-this.minCust)) + this.minCust;
     },   
     getCookiesPerHour: function(){
-        return Math.floor(this.getRandomCustomer()* this.avgCookieSale);
-    } 
+        for (var i = 0; i < timeOfDay.length; i++) {
+            this.cookieResults.push( 
+                Math.floor(this.getRandomCustomer()* this.avgCookieSale)
+            )    
+        }
+    },
+    cookieResults: [] 
 }    
+pioneerSquare.getCookiesPerHour();
+document.getElementById("pioneer-Square").innerHTML = pioneerSquare.cookieResults;
+console.log(pioneerSquare.cookieResults)
+
+
 
 var portlandAirport = {
     location: "Portland Airport",
@@ -65,9 +71,32 @@ var pearlDistrict = {
     maxCust: 24,
     avgCookieSale: 2.6,
     getRandomCustomer: function(){
-        return Math.floor(Math.random()) * (this.maxCust-this.minCust)) + this.minCust;
+        return Math.floor(Math.random() * (this.maxCust-this.minCust)) + this.minCust;
     },
     getCookiesPerHour: function(){
         return Math.floor(this.getRandomCustomer()* this.avgCookieSale);
     }   
 }
+
+
+
+
+
+
+
+// var cookieStands = []
+// cookieStand.push({location: "Pioneer Square", minCust: "17", maxCust: "88", avgCookieSale:"5.2"});
+// cookieStand.push({location: "Portland Airport", minCust: "6", maxCust: "24", avgCookieSale: "1.2"});
+// cookieStand.push({location: "Washington Square", minCust: "11", maxCust: "38", avgCookieSale: "1.9"});
+// cookieStand.push({location: "Sellwood", minCust: "20", maxCust: "48", avgCookieSale: "3.3"});
+// cookieStand.push({location: "Pearl District", minCust: "3", maxCust: "24", avgCookieSale: "2.6"});
+
+
+
+
+// var cookieStands = [];
+// cookieStand = ({pioneerSquare});
+// cookieStand = ({portlandAirport});
+// cookieStand = ({washingtonSquare});
+// cookieStand = ({sellwood});
+// cookieStand = ({pearlDistrict});
