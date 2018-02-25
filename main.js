f1e8347a46517dbf53d517d55223af2feba4d727
Davidchhing <davidhchhing@gmail.com>
2018-02-25 15:19:38 -0800

    in the middle of making table row header for time of day

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 
 var timeOfDay = ['10AM', '11AM', '12pm', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
+// var storeHTML = document.getElementById("locations");
+// var rowHeaders = document.createElement("tr");
+// table.appendChild(rowHeaders);
+
+// for (var = index; index < timeOfDay.length; index++) {
+//     var cells = document.createElement("td");
+//     cells.textContent = timeOfDay[index]; 
+//     rowHeaders.appendChild(cell);
+// }
+
+// var store = new CookieStores('Pioneer Square', 17, 88, 5.2);
+
+// var cell = document.createElement("td");
+// cell.textContent = store.name;
+
+
+
 function CookieStores(name, minCust, maxCust, avgCookieSale, getRandomCustomer,getCookiesPerHour) {
     this.name = name;
     this.minCust = minCust;
@@ -24,6 +41,9 @@ storeLocations.push(new CookieStores('Pearl District', 3, 24, 2.6));
 
 function makeTable() {
     var storeHTML = document.getElementById("locations").firstElementChild;
+    console.log(storeHTML);
+    storeHTML.innerHTML = "";
+    console.log(storeHTML);
     for (var storeIndex = 0; storeIndex < storeLocations.length; storeIndex++) {
         storeHTML.innerHTML += "<tr><th>" + storeLocations[storeIndex].name + "</th></tr>";
         var totalCookies = 0;  
@@ -43,11 +63,17 @@ function getStoreInfo(form) {
     var newAverageCookieSale = form["average-cookie-sale"].value;
     storeLocations.push(new CookieStores(storeName, minCustomers, maxCustomers, newAverageCookieSale));
     makeTable();
+    form.reset();
 }
 
 makeTable();
 
-// function validateNewStoreInfo(fieldset) {
+
+
+
+
+
+// function validateNewStoreInfo(form) {
 //     var allFieldsFilled = true;
 //     for (var index = 0; index < fieldset.elements.length; index++) {
 //          if (fieldset.elements[index].value =="") {
